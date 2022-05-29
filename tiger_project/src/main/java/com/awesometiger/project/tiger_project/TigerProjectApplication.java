313cac2a1678c831d321cda0feffbba3b5d8b9cb
Ronald Suplina <tyger995@gmail.com>
2022-05-29 13:57:36 +0100

    Base Project

---
+++ tiger_project/src/main/java/com/awesometiger/project/tiger_project/TigerProjectApplication.java
@@ -0,0 +1,14 @@
+package com.awesometiger.project.tiger_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TigerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TigerProjectApplication.class, args);
+		System.out.println("Test ");
+	}
+
+}
---
+++ tiger_project/src/test/java/com/awesometiger/project/tiger_project/TigerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awesometiger.project.tiger_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TigerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
